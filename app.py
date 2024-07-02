731dbbc38e2cfc74ecbe5e5bf4b8efdf8c4cd2ba
C3lin322 <celinewitono@gmail.com>
2024-07-02 12:50:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
     
-    @app.route("/", methods=["GET","POST"])
-
-def index():
-    return(render_template("index.html"))
-
 
 if __name__ == "__main__":
     app.run()
